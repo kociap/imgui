bdfded8bbe6a84d1dcf7a53860e5d182c67a4260
Piotr Kocia <p.kocia@wp.pl>
2020-10-17 17:38:32 +0200

    fixed misplaced const

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -1091,7 +1091,7 @@ namespace ImGui {
             ImVec2 pad(ImMax(1.0f, IM_FLOOR(square_sz / 3.6f)), ImMax(1.0f, IM_FLOOR(square_sz / 3.6f)));
             window->DrawList->AddRectFilled(check_bb.Min + pad, check_bb.Max - pad, check_col, style.FrameRounding);
         } else if(*v) {
-            f32 pad const = ImMax(1.0f, IM_FLOOR(square_sz / 6.0f));
+            f32 const pad = ImMax(1.0f, IM_FLOOR(square_sz / 6.0f));
             RenderCheckMark(window->DrawList, check_bb.Min + ImVec2(pad, pad), check_col, square_sz - pad * 2.0f);
         }
 
